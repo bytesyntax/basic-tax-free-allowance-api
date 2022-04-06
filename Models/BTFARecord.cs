8f4960248ad37e779cdc37672dbc1447934b4e71
Ola Törnquist <ola.tornquist@gmail.com>
2022-04-06 15:26:55 +0200

    split up the classes into models folder

---
+++ Models/BTFARecord.cs
@@ -0,0 +1,66 @@
+﻿namespace BasicTaxFreeAllowanceApi.Models
+{
+    class BTFARecord
+    {
+        private static long Records = 0;
+
+        private Double _Income;
+        public Double Income
+        {
+            get
+            {
+                return _Income;
+            }
+            set
+            {
+                _Income = value;
+                Btfa = calculateBtfa(_Income);
+            }
+        }
+        public string Id { get; private set; }
+
+        public Double Btfa { get; private set; }
+
+
+        public BTFARecord()
+        {
+            System.Threading.Interlocked.Increment(ref Records);
+            Id = "BESK2022_" + Records;
+        }
+
+        private Double calculateBtfa(Double income)
+        {
+            Double Result;
+            const Double PBB = 48_300;
+
+            // Round down to nearest 100
+            income = Math.Round(income / 100, MidpointRounding.ToNegativeInfinity) * 100;
+
+            switch (income)
+            {
+                case <= 0.99 * PBB:
+                    Result = 0.423 * PBB;
+                    break;
+                case <= 2.72 * PBB:
+                    Result = 0.423 * PBB + 0.2 * (income - 0.99 * PBB);
+                    break;
+                case <= 3.11 * PBB:
+                    Result = 0.77 * PBB;
+                    break;
+                case <= 7.88 * PBB:
+                    Result = 0.77 * PBB - 0.1 * (income - 3.11 * PBB);
+                    break;
+                case > 7.88 * PBB:
+                    Result = 0.293 * PBB;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            Result = Math.Round(Result / 100, MidpointRounding.ToPositiveInfinity) * 100;
+            Result = Math.Min(income, Result);
+
+            return Result;
+        }
+    }
+}
---
+++ Models/BTFARepository.cs
@@ -0,0 +1,45 @@
+﻿namespace BasicTaxFreeAllowanceApi.Models
+{
+    class BTFARepository
+    {
+        private readonly Dictionary<string, BTFARecord> _records = new();
+
+        public void Create(BTFARecord record)
+        {
+            if (record == null)
+            {
+                return;
+            }
+
+            _records[record.Id] = record;
+        }
+
+        public BTFARecord GetById(string id)
+        {
+            if (_records.ContainsKey(id))
+                return _records[id];
+            return null;
+        }
+
+        public List<BTFARecord> GetAll()
+        {
+            return _records.Values.ToList();
+        }
+
+        public void Update(BTFARecord record)
+        {
+            var existingRecord = GetById(record.Id);
+            if (existingRecord == null)
+            {
+                return;
+            }
+
+            _records[record.Id] = record;
+        }
+
+        public void Delete(string id)
+        {
+            _records.Remove(id);
+        }
+    }
+}
